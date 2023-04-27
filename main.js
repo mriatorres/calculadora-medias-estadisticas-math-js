9c0531f4b2fa2cc1020d023f877da554d8fd5058
Maria <mariaf7458@gmail.com>
2023-04-27 00:25:37 -0500

    Se agregaro funcionalidad a calculadoras medias

--- main.js
+++ main.js
@@ -1,30 +1,164 @@
-//scroll entre secciones
 
-//Datos usuario
-function ingreso () {
-    const valor = prompt('Ingrese numeros separados por coma');
-    const separado = valor.split(',');
+
+//input usuario
+
+function calcularAritmetica() {
+   
+
+    // Selecting the input element and get its value 
+    let inputVal = document.getElementById("inputAritmetica").value;
+    console.log(inputVal);
+    const separado = inputVal.split(",");
 
     let numeros = [];
 
     separado.forEach(element => {
         numeros.push(Number(element));
-    })
-        return numeros;
+    });
+    
+    //sumar datos
+    const sumaDatos = MediasEstadistica.sumarDatos(numeros);
+    //division datos
+    const divisionDatos = MediasEstadistica.dividirSumDatosEntreNumDatos(sumaDatos, numeros);
+    //resultado a string
+    let spanArit = document.createElement('span');
+    document.getElementById('parrafoArit').appendChild(spanArit);
+    const resultadoAritS = divisionDatos.toString();
+    spanArit.innerHTML = ('= '+ resultadoAritS); 
+    
+}   
+
+function calcularPonderada(){
+    
+
+    // Selecting the input element and get its value 
+    let inputVal = document.getElementById("moderada").value;
+    // Displaying the value
+    const separado = inputVal.split(",");
+
+    let numeros1 = [];
+    console.log(numeros1);
+
+    separado.forEach(element => {
+        numeros1.push(Number(element));
+    });
+
+    let inputVal2 = document.getElementById("moderada2").value;
+
+    const separado2 = inputVal2.split(",");
+
+    let numeros2 = [];
+
+    separado2.forEach(element => {
+        numeros2.push(Number(element));
+    });
+
+    console.log(numeros2);
+
+    //datos por peso
+    const datosPorPeso = MediasEstadistica.multiDatosPorPeso(numeros1, numeros2);
+    console.log(datosPorPeso);
+    //sumar pesos
+    const sumaDatosPorPesos = MediasEstadistica.sumarDatos(datosPorPeso);
+    console.log(sumaDatosPorPesos);
+    //sumar pesos
+    const sumarPesos = MediasEstadistica.sumarDatos(numeros2);
+    //Dividir pesos entre datos
+    const pesosEntreDatos = MediasEstadistica.valor1SobreValor2(sumaDatosPorPesos, sumarPesos);
+    console.log(pesosEntreDatos);
+    //Resultado a string
+    let spanPond = document.createElement('span');
+    document.getElementById('parrafoPond').appendChild(spanPond);
+    const resultadoPondS = pesosEntreDatos.toString();
+    spanPond.innerHTML = ('= '+ resultadoPondS); 
+    
 }
 
-//Botones calcular
-const btnAritmetica = document.getElementById('btnAritmetica');
-btnAritmetica.addEventListener('click', calcularAritmetica());
+function calcularCuadratica(){
+    let inputVal = document.getElementById("cuadratica").value;
+    // Displaying the value
+    console.log(inputVal);
+    const separado = inputVal.split(",");
 
+    let numeros = [];
 
-function calcularAritmetica (){
-    console.log('boton aritmetica oprimido');
+    separado.forEach(element => {
+        numeros.push(Number(element));
+    });
+
+    //Elevar datos al cuadrado
+    const datosAlCuadrado = MediasEstadistica.datosAlcuadrado(numeros);
+    //Sumar datos al cuadrado
+    const sumarCuadrados = MediasEstadistica.sumarDatos(datosAlCuadrado);
+    //Dividir resultado suma entre numero de datos
+    const dividirDatosCuad = MediasEstadistica.dividirSumDatosEntreNumDatos(sumarCuadrados, numeros);
+    //Sacar raiz datos al cuadrado
+    const raiz = MediasEstadistica.raizCuadradaDato(dividirDatosCuad);
+
+    //Resultado a string
+    let spanCuad = document.createElement('span');
+    document.getElementById('parrafoCuad').appendChild(spanCuad);
+    const resultadoCuadS = raiz.toString();
+    spanCuad.innerHTML = ('= '+ resultadoCuadS); 
+
+    
+}
+
+function calcularArmonica (){
+    let inputVal = document.getElementById("armonica").value;
+    // Displaying the value
+    console.log(inputVal);
+    const separado = inputVal.split(",");
+
+    let numeros = [];
+
+    separado.forEach(element => {
+        numeros.push(Number(element));
+    });
+
+    //Se crean fracciones
+    const fracciones = MediasEstadistica.crearFraccionesArmonicas(numeros);
+    console.log(fracciones);
+    //sumar resultado fracciones
+    const sumaFracciones = MediasEstadistica.sumarDatos(fracciones);
+    console.log(sumaFracciones); 
+    //Dividir numero de datos entre suma fracciones
+    const divisionDatosArm = Number(MediasEstadistica.dividirNumDatosEntreSumDatos(numeros, sumaFracciones).toFixed(2));
+    console.log(divisionDatosArm);
+    //Resultado a string
+    let spanArm = document.createElement('span');
+    document.getElementById('parrafoArm').appendChild(spanArm);
+    const resultadoArmS = divisionDatosArm.toString();
+    spanArm.innerHTML = ('= '+ resultadoArmS); 
 
 }
 
-const btnPonderada = document.getElementById('btnPonderada');
-const btnCuadratica = document.getElementById('btnCuadratica');
+function calcularGeometrica(){
+    let inputVal = document.getElementById("geometrica").value;
+    console.log(inputVal);
+    const separado = inputVal.split(",");
+
+    let numeros = [];
+
+    separado.forEach(element => {
+        numeros.push(Number(element));
+    });
+
+    //multiplicar datos
+    const multiplicarDatos = MediasEstadistica.multiplicarDatos(numeros);
+    console.log(multiplicarDatos);
+    //hallar raiz 
+    const hallarRaiz = Number(MediasEstadistica.hallarNRaizConNumDatos(multiplicarDatos, numeros).toFixed(2));
+    console.log(hallarRaiz);
+
+     //resultado a string
+     let spanGeo = document.createElement('span');
+     document.getElementById('parrafoGeo').appendChild(spanGeo);
+     const resultadoGeoS = hallarRaiz.toString();
+     spanGeo.innerHTML = ('= '+ resultadoGeoS); 
+}
+
+
 
 // function ingreso (valor) {
 // valor = document.getElementById('inputUser').values;
